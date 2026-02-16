cf95557764aa037d9e7545eb2a8a5c1ea9fe40b3
Neerajan <nirajanbohora2001@gmail.com>
2026-02-16 09:50:11 +0545

    update

--- src/App.vue
+++ src/App.vue
@@ -50,4 +50,15 @@ html, body, #app {
   height: 100%;
   overflow: hidden;
 }
+
+.app {
+width: 100%;
+height: 100vh;
+
+display: flex;
+align-items: center;
+justify-content: center;
+
+background: linear-gradient(135deg, #667eea 0%), #764ba2 100%;
+}
 </style>
